86b654355ffed81ac5741fd7151149adbc97f344
Tim <tim.herzig@hotmail.com>
2021-09-09 14:15:11 +0200

    fade_in_out, not yet working

---
+++ misc/fade_in_out.py
@@ -0,0 +1,20 @@
+import os
+
+location = '/home/tim/Documents/Datasets/hu_dysarthria_final_data/final_data'
+
+speakers = os.listdir(location)
+
+os.system('ffmpeg -f lavfi -i anullsrc=r=16000:cl=mono -t 0.1 -q:a 9 -acodec libmp3lame 100msSilence.mp3') #('ffmpeg -filter_complex aevalsrc=0 -t 0.1 -ar 16000 100msSilence.mp3')
+
+for speaker in speakers:
+    loc = os.path.join(location, speaker)
+    if os.path.isdir(loc) and speaker[0] == 'M' or speaker[0] == 'F':
+        loc = os.path.join(loc, 'audio_1')
+        if os.path.isdir(loc):
+            audio_files = os.listdir(loc)
+            for af in audio_files:
+                if af.endswith('.mp3'):
+                    a = os.path.join(loc, af)
+                    a_copy = os.path.join(loc, 'copy_' + af)
+                    print(a_copy)
+                    os.system('ffmpeg -i "concat:100msSilence.mp3|{}|100msSilence.mp3" -c {}'.format(a, a_copy))
