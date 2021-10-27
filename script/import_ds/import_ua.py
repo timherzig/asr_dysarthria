52049a910bce630d69c90a381c0149e5f974f99c
Tim Herzig <tim.herzig@hotmail.com>
2021-10-27 13:39:50 +0200

    ua import change

--- script/import_ds/import_ua.py
+++ script/import_ds/import_ua.py
@@ -94,29 +94,26 @@ def import_ua(location, test_train):
                         {'id': id, 'target': target, 'speech': speech}, ignore_index=True)
 
         # FOR CONTROL SPEAKERS
-        cspeakers = os.listdir(os.path.join(location, 'audio', 'control'))
-        for speaker in cspeakers:
-            cdf = pd.DataFrame(columns=['id', 'speech', 'target'])
+        # cspeakers = os.listdir(os.path.join(location, 'audio', 'control'))
+        # for speaker in cspeakers:
+        #     cdf = pd.DataFrame(columns=['id', 'speech', 'target'])
 
-            files = os.listdir(os.path.join(
-                location, 'audio', 'control', speaker))
-            for file in files:
-                id, block, word_id = [file.split('_')[i] for i in (0, 1, 2)]
-                word = words.loc[words['FILE NAME'] == word_id]
-                if word.empty:
-                    word = words.loc[words['FILE NAME'] == (block + '_' + word_id)]
-                target = str(word.iloc[0]['WORD'])
-                speech = speech_file_to_array(
-                    os.path.join(location, 'audio', 'control', speaker, file))
+        #     files = os.listdir(os.path.join(
+        #         location, 'audio', 'control', speaker))
+        #     for file in files:
+        #         id, block, word_id = [file.split('_')[i] for i in (0, 1, 2)]
+        #         word = words.loc[words['FILE NAME'] == word_id]
+        #         if word.empty:
+        #             word = words.loc[words['FILE NAME'] == (block + '_' + word_id)]
+        #         target = str(word.iloc[0]['WORD'])
+        #         speech = speech_file_to_array(
+        #             os.path.join(location, 'audio', 'control', speaker, file))
 
-                if block == ('B1' or 'B3'):
-                    train_ds = train_ds.append(
-                        {'id': id, 'target': target, 'speech': speech}, ignore_index=True)
-                elif block == 'B2':
-                    test_ds = test_ds.append(
-                        {'id': id, 'target': target, 'speech': speech}, ignore_index=True)
+        #         if block == ('B1' or 'B3'):
+        #             train_ds = train_ds.append(
+        #                 {'id': id, 'target': target, 'speech': speech}, ignore_index=True)
+        #         elif block == 'B2':
+        #             test_ds = test_ds.append(
+        #                 {'id': id, 'target': target, 'speech': speech}, ignore_index=True)
         
         return Dataset.from_pandas(train_ds), Dataset.from_pandas(test_ds)
-
-
-import_ua('/home/tim/Documents/Datasets/uaspeech', False)
